04ac283dafd86ca94b309558a7c0de3fc1062da4
Sam Georges <sam@daftspunk.com>
2013-05-17 19:42:11 +1000

    Refit schema to structure

--- updates/structure.php
+++ updates/structure.php
@@ -7,7 +7,6 @@ $table = Db_Structure::table('profile_friends');
 	$table->column('mutual', db_bool);
 	$table->footprints(false);
 	$table->column('deleted_at', db_datetime);
-	$table->save();
 
 $table = Db_Structure::table('profile_wall');
 	$table->primary_key('id');
@@ -15,4 +14,3 @@ $table = Db_Structure::table('profile_wall');
 	$table->column('user_id', db_number)->index();
 	$table->column('message', db_text);
 	$table->footprints(false);
-	$table->save();
